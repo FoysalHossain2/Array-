3e7da395fd5d342a01e149b202fc863f9491e207
FoysalHossain2 <fh375170@gmail.com>
2024-01-19 21:49:02 +0600

    JS code

---
+++ JS/main.js
@@ -0,0 +1,12 @@
+// const muArr = [1,2,3,4,5,6];
+
+// // console.log(muArr[0]);
+
+// // muArr.push(7);
+// // muArr.pop();
+// // muArr.unshift(10);
+// // muArr.shift()
+
+
+// console.log(muArr);
+
